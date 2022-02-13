d8bb0961f2ca29990a0a8faef0af032990472b47
Oniel Santos Álvarez <59784349+Oniel1721@users.noreply.github.com>
2022-02-13 18:40:11 -0400

    update

--- scripts/index.js
+++ scripts/index.js
@@ -1,54 +1,50 @@
-// Variables
-const $form = document.querySelector('#poke-form')
-const $input = document.querySelector('#poke-input')
-const $pokeSprite = document.querySelector('#poke-sprite')
-const $pokeName = document.querySelector('#poke-name')
-const $pokeTypes = document.querySelector('#poke-types')
+const $form = document.querySelector("#poke-form")
+const $input = document.querySelector("#poke-input")
 
-// Función para actualizar la informacion del pokemon
-const showPokemonData = (data) => {
+const $pokeSprite = document.querySelector("#poke-sprite")
+const $pokeName = document.querySelector("#poke-name")
+const $pokeTypes = document.querySelector("#poke-types")
+
+const showPokemonData = (data)=>{
     $pokeSprite.src = data.sprite
-    console.log($pokeName)
     $pokeName.textContent = `#${data.id} ${data.name}`
     $pokeTypes.innerHTML = ''
-    data.types.forEach(({type})=>{
-        const $type = document.createElement('div')
+    data.types.forEach((elements)=>{
+        const $type = document.createElement("div")
         $type.classList.add('type')
-        $type.classList.add(`type-${type.name}`)
-        $type.textContent = type.name
+        $type.classList.add('type-'+elements.type.name)
+        $type.textContent = elements.type.name
         $pokeTypes.appendChild($type)
     })
 }
 
-const xhr = new XMLHttpRequest
 
-// Funcion para obtener los datos del pokemon deseado
 const getPokemonData = (name)=>{
-    fetch('https://pokeapi.co/api/v2/pokemon/'+name)
-    .then(res=>{
-        if(res.ok){
-            return res.json()
+    fetch("https://pokeapi.co/api/v2/pokemon/"+name)
+    .then((response)=>{
+        if(response.ok){
+            return response.json()
         }
     })
-    .then(json=>{
+    .then((json)=>{
         if(!json) return;
         const neededData = {
             name: json.name,
-            types: json.types,
             id: json.id,
+            types: json.types,
             sprite: json.sprites.front_default
         }
         showPokemonData(neededData)
     })
-    .catch(err => {
-        console.error(err)
+    .catch((error)=>{
+        console.log(error)
     })
 }
 
-// Evento para detectar la entrega del formulario
-document.addEventListener('submit', (event)=>{
+document.addEventListener("submit", (event)=>{
     event.preventDefault()
     if(event.target === $form){
         getPokemonData($input.value.toLowerCase())
     }
 })
+
